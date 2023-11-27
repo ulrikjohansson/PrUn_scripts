3d6666a13cd87921731f3a721b2ffe303b1e1923
Ulrik Johansson <ulrik.johansson@gmail.com>
2023-11-27 09:40:10 +0000

    filter by POS

--- HAL9666/AuctionMasterBot.py
+++ HAL9666/AuctionMasterBot.py
@@ -28,8 +28,8 @@ CachedSellersData:  Iterable[dict[str, str]] = {}
 FioInventoryUrl = "https://rest.fnar.net/csv/inventory?group={group}&apikey={apikey}"
 FioInventoryShipyardGroup = "41707164"
 FioInventoryEv1lGroup = "83373923"
-CachedShipyardInventories: dict[str, dict[str, int]] = {}
-CachedEv1lInventories: dict[str, dict[str, int]] = {}
+CachedShipyardInventories: dict[str, dict[str, tuple[str,int]]] = {}
+CachedEv1lInventories: dict[str, dict[str, tuple[str,int]]] = {}
 ShipPartTickers = (
     "BR1",
     "BR2",  #bridges
@@ -468,7 +468,7 @@ async def help(ctx):
       "$bid [price]\nPlaces a new bid. Examples:\n$bid 4mil\nbid 4.25mil\n")
   await ctx.send("$status\nShows current auction status")
 
-async def updateInventory(groupId: str, inventory: dict[str, dict[str,int]]):
+async def updateInventory(groupId: str, inventory: dict[str, dict[str,list[tuple[str,int]]]]):
   fioUrl = FioInventoryUrl.format(
       apikey=os.getenv("FIO_API_KEY"),
       group=groupId)
@@ -483,34 +483,56 @@ async def updateInventory(groupId: str, inventory: dict[str, dict[str,int]]):
     if row["Username"] not in inventory:
       inventory[row["Username"]] = {}
     if row["Ticker"] not in inventory[row["Username"]]:
-      inventory[row["Username"]][row["Ticker"]] = 0
-    inventory[row["Username"]][row["Ticker"]] += int(row["Amount"])
+      inventory[row["Username"]][row["Ticker"]] = []
+    inventory[row["Username"]][row["Ticker"]].append((row["NaturalId"], int(row["Amount"])))
 
-async def findInInventory(ticker: str, inventory: dict[str, dict[str,int]], shouldReturnAll: bool = False)->list[tuple[str, int]]:
-  result: list[tuple[str, int]] = []
+async def findInInventory(ticker: str, inventory: dict[str, dict[str,list[tuple[str,int]]]], shouldReturnAll: bool = False)->list[tuple[str, int]]:
+  result: list[tuple[str, list[tuple[str,int]]]] = []
+  # filter for only ticker we want
   for (user, inv) in inventory.items():
     if ticker in inv:
       result.append((user, inv[ticker]))
 
   if not shouldReturnAll:
-    sellers = getSellers(ticker)
+    sellersData = getSellerData(ticker)
+    sellers = [s for s in sellersData.keys()]
     print("Sellers:", str(sellers))
-    result = [(u, a) for (u, a) in result if u in sellers]
+    seller_filtered_result = [x for x in result if x[0] in sellers]
 
-  return sorted(result, key=lambda x: x[1])[::-1]
+    pos_filtered_result: list[tuple[str,list[tuple[str,int]]]] = []
+
+    for (user, inv_rows) in seller_filtered_result:
+      filtered_inv_rows = [x for x in inv_rows if x[0] in sellersData[user] or len(sellersData[user]) == 0]
+      if len(filtered_inv_rows) > 0:
+        pos_filtered_result.append((user, filtered_inv_rows))
+
+    result = pos_filtered_result
+      
+  summed_inventories: list[tuple[str,int]] = []
+  # sum up amounts from all remaining locations
+  for (user, inv_rows) in result:
+    amount = sum([x[1] for x in inv_rows])
+    if amount > 0:
+      summed_inventories.append((user, amount))
 
 
-def getSellers(ticker: str) -> list[str]:
+
+  return sorted(summed_inventories, key=lambda x: x[1])[::-1]
+
+
+def getSellerData(ticker: str) -> dict[str,list[str]]:
   global CachedSellersData
-  result: list[str] = []
+  result = {}
   response = requests.get(OfferingsCsvUrl)
   if response.status_code == 200:
-    CachedSellersData = csv.DictReader(response.text.split("\r\n"))
+    CachedSellersData = list(csv.DictReader(response.text.split("\r\n")))
   if CachedSellersData:
-    result = [
-        row["Seller"].upper() for row in CachedSellersData
-        if row["MAT"] == ticker
-    ]
+    result: dict[str,list[str]] = {}
+    for row in CachedSellersData:
+      pos_list = [x for x in row.get("POS", "").split(',') if x != ""]
+      if row["MAT"] == ticker:
+        result[row['Seller'].upper()] = pos_list
+
   return result
 
 async def whohas(ctx: Any, ticker: str, shouldReturnAll: bool = False) -> list[tuple[str, int]]:
@@ -526,7 +548,7 @@ async def whohas(ctx: Any, ticker: str, shouldReturnAll: bool = False) -> list[t
   inventory = CachedShipyardInventories if isShipPartTicker else CachedEv1lInventories
   try:
     await updateInventory(groupId=group, inventory=inventory)
-  except Exception:
+  except Exception as e:
     await ctx.reply(
         "Error updating inventory from FIO. Falling back to cached data"
     )
